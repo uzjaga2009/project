7bcdb489a6fa4c41ac9516ce6c98536f9ac0392d
Jahongir <exemple@gmail.com>
2022-06-16 13:22:39 +0300

    javascript

---
+++ script.js
@@ -0,0 +1,157 @@
+// const numberOfFilms = +prompt('Qancha kino kordingiz?', '')
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// const a = prompt('Korgan kinolaringizdan biri?', ''),
+//       b = prompt('Qancha ballga baholaysiz?', ''),
+//       c = prompt('Korgan kinolaringizdan biri?', ''),
+//       d = prompt('Qancha ballga baholaysiz?', '');
+
+//     personalMovieDB.movies[a] = b;
+//     personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
+
+
+// SHARTLAR \\
+
+// if (4 == 9) {
+//     console.log('Ok!');
+// } else {
+//     console.log('Error!');
+// }
+
+// if (4 == 4) {
+//     console.log('Ok!');
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Error!');
+// } else if (num > 100) {
+//     console.log('Mnoga');
+// } else {
+//     console.log('Ok!');
+// }
+
+// (num === 50) ? console.log('Ok!') : console.log('Error!');
+
+// const num = 50;
+
+// switch (num) {
+//     case 49:
+//         console.log('Xato');
+//         break;
+//     case 100:
+//         console.log('Xato');
+//         break;
+//     case 50:
+//         console.log('Togri');
+//         break;
+//     default:
+//         console.log('Oxshamadi');
+//         break;
+// }
+
+
+// Mantiqiy operatorlar \\
+
+// const hamburger = true;
+// const fries = true;
+
+// if (hamburger && fries) {
+//     console.log('Men rozi!');
+// }
+
+// console.log(hamburger && fries);
+
+// const hamburger = 2;
+// const fries = 1;
+
+// if (hamburger === 3 && fries) {
+//     console.log('Hamma rozi!');
+// } else {
+//     console.log('Biz kettik!');
+// }
+
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 0;
+
+// console.log(hamburger ===3 && cola && fries);
+
+// if (hamburger === 3 && cola && fries) {
+//     console.log('Hamma rozi!');
+// } else {
+//     console.log('Biz kettik!');
+// }
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && 'bfseylbfvybgtfbgsby');
+
+// if (hamburger === 3 && cola === 1 && fries) {
+//     console.log('Hamma rozi!');
+// } else {
+//     console.log('Biz kettik!');
+// }
+
+// const hamburger = 3;
+// const fries = 0;
+// const cola = 0;
+
+// if (hamburger ===3 || cola || fries) {
+//     console.log('Hamma rozi!');
+// } else {
+//     console.log('Biz kettik!');
+// }
+
+// const hamburger = 0;
+// const fries = 0;
+// const cola = 0;
+
+// if (hamburger || cola || fries) {
+//     console.log('Hamma rozi!');
+// } else {
+//     console.log('Biz kettik!');
+// }
+
+// console.log(hamburger || cola || fries);
+
+
+// const hamburger = 0;
+// const fries = null;
+// const cola = 0;
+
+// if (hamburger || cola || fries) {
+//     console.log('Hamma rozi!');
+// } else {
+//     console.log('Biz kettik!');
+// }
+
+// console.log(hamburger || cola || fries);
+
+// let johnReport, alexReport, samReport, mariaReport = 'done'
+
+// console.log(johnReport || alexReport || samReport || mariaReport);
+
+// const hamburger = 0;
+// const fries = null;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger === 3|| cola === 2 || nuggets || fries === 3) {
+//     console.log('Hamma rozi!');
+// } else {
+//     console.log('Biz kettik!');
+// }
+
+// console.log(hamburger === 3|| cola === 2 || nuggets || fries === 3);
